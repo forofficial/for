f9a84828dff52d7b0ce44b812f4252069ea260f5
forofficial <118924303+forofficial@users.noreply.github.com>
2023-05-26 13:04:55 +0530

    Add files via upload

---
+++ quicksortrand.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include <cstdlib>
+#include<time.h>
+#include<iomanip>
+#include<omp.h>
+using namespace std;
+
+void quicksort(int arr[], int st, int end)
+{
+    if(st < end)
+    {
+    	srand(time(NULL));
+        int pivot = arr[st + rand() % (end - st+1)];
+        int i = st-1;
+        int j = st;
+
+        for(int var = j; var<end; var++)
+        {
+            if(arr[var] < pivot)
+            {
+                i++; 
+                int temp = arr[i]; 
+                arr[i] = arr[var];
+                arr[var] = temp;
+            }
+        }
+
+        // swap i+1 and end
+        int temp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = temp;
+
+        i++;
+
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            quicksort(arr,st,i-1);
+
+            #pragma omp section
+            quicksort(arr,i+1,end);
+        }
+    
+    }
+
+}
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements: "; 
+    cin>>n;
+
+    int arr[n] = {0};
+    
+    for(int i=0; i<n; i++){
+    	cout<<"enter element "<<i+1<<":-";
+    	cin>>arr[i];
+	}
+
+    cout<<"Array: ";   
+    for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+    cout<<endl;
+
+	clock_t strt =clock();
+    quicksort(arr,0,n-1);
+	clock_t end =clock();
+    cout<<"Sorted Array: ";
+    for(int i=0; i<n; i++) cout<<arr[i]<<" ";
+    cout<<"\ntime taken in ms:-"<<(double)(end-strt);
+    return 0;
+}
